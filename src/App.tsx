5df5bc5bf71b96fd01a25811db717b8e0c883dd1
Tatu Soininen <taatusti@gmail.com>
2023-05-05 18:18:56 +0300

    Created Droppable - basic component for drop container

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { DndContext } from '@dnd-kit/core';
-
+import Droppable from './Components/Droppable/Droppable';
 import './App.css';
 
 const App = (): JSX.Element => {
   return (
     <div className="App">
       <DndContext>
-
+        <Droppable text="Drop Container" />
       </DndContext>
     </div>
   );
---
+++ src/Components/Droppable/Droppable.tsx
@@ -0,0 +1,26 @@
+import { FC } from 'react';
+import { useDroppable } from '@dnd-kit/core';
+import './Droppable.css';
+
+interface DroppableProps {
+    text: string;
+    children?: any;
+}
+
+const Droppable: FC<DroppableProps> = (props): JSX.Element => {
+    const { isOver, setNodeRef } = useDroppable({
+        id: 'droppable',
+    });
+    const style = {
+        color: isOver ? 'green' : undefined
+    };
+
+    return (
+        <div className='drop-container' ref={setNodeRef} style={style}>
+            <span className='text'>{props.text}</span>
+            {props.children}
+        </div>
+    );
+}
+
+export default Droppable;
